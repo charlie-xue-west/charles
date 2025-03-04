3382008e5cdbe733482c1abee030726faf80937f
Darkflam3master <x.charlie42@gmail.com>
2025-03-04 12:05:11 -0600

    initial atoms setup

---
+++ src/app/components/atoms/create-account-button/CreateAccountButton.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export const CreateAccountButton = () => {
+  const handleClick = () => {};
+  return <button onClick={handleClick}>Create Account</button>;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { CreateAccountButton } from "@components";
+
 export default function Home() {
-  return <div className="">Welcome to Charles</div>;
+  return (
+    <>
+      <div className="">Welcome to Charles</div>
+      <CreateAccountButton />
+    </>
+  );
 }
