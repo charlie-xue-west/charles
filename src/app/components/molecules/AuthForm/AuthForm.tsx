088fcac86455b71335b641c529e0a7809c421d81
Darkflam3master <x.charlie42@gmail.com>
2025-03-19 15:15:43 -0500

    handle data change and submit

--- src/app/components/molecules/AuthForm/AuthForm.tsx
+++ src/app/components/molecules/AuthForm/AuthForm.tsx
@@ -2,6 +2,7 @@
 
 import {
   Box,
+  Button,
   createTheme,
   outlinedInputClasses,
   TextField,
@@ -9,9 +10,18 @@ import {
   ThemeProvider,
   useTheme,
 } from "@mui/material";
+import { useFetchData } from "../../../lib/customHooks";
+import { useState } from "react";
 
 type AuthFormProps = {
   formType: "signup" | "login";
+};
+
+type FormData = {
+  userName: string;
+  password: string;
+  email?: string;
+  dateOfBirth?: string;
 };
 
 const customTheme = (outerTheme: Theme) =>
@@ -40,39 +50,81 @@ const customTheme = (outerTheme: Theme) =>
 
 export const AuthForm = ({ formType }: AuthFormProps) => {
   const outerTheme = useTheme();
+  const [formData, setFormData] = useState<FormData>({
+    userName: "",
+    password: "",
+  });
+  const { error, isLoading, fetchData } = useFetchData();
+
+  const handleOnsubmit = () => {
+    const { userName, password, email, dateOfBirth } = formData;
+    const userData = {
+      userName,
+      password,
+      ...(email && { email }),
+      ...(dateOfBirth && { dateOfBirth }),
+    };
+
+    fetchData(`http://localhost:3001/auth/${formType}`, "Post", userData);
+  };
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <Box
       component="form"
-      className="flex flex-col bg-white p-16 rounded-lg gap-4"
+      className="flex flex-col bg-gray-200 p-16 rounded-lg gap-4"
+      onSubmit={handleOnsubmit}
     >
+      <h2 className="">Welcome Traveler</h2>
       <ThemeProvider theme={customTheme(outerTheme)}>
         <TextField
           required
+          name="userName"
           color="secondary"
-          id="user-name"
           label="User Name"
           helperText="required"
+          onChange={handleOnChange}
         />
         {formType === "signup" && (
           <TextField
             required
+            name="email"
             color="secondary"
-            id="user-email"
             label="Email"
             helperText="required"
+            onChange={handleOnChange}
           />
         )}
         <TextField
           required
+          name="password"
           color="secondary"
-          id="user-password"
           label="Password"
           helperText="required"
+          onChange={handleOnChange}
         />
 
         {formType === "signup" && (
-          <TextField color="secondary" id="user-dob" label="Date of Birth" />
+          <TextField
+            name="dateOfBirth"
+            color="secondary"
+            label="Date of Birth"
+            onChange={handleOnChange}
+          />
         )}
+        <Button type="submit" variant="contained" color="secondary">
+          {formType === "signup" ? "Sign Up" : "Log In"}
+        </Button>
       </ThemeProvider>
     </Box>
   );
