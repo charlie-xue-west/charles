860bd9059efdcf1a0b6d2865f68017b94e433df8
Darkflam3master <x.charlie42@gmail.com>
2025-03-19 10:17:04 -0500

    clean up

--- src/app/components/atoms/create-account-button/CreateAccountButton.tsx
+++ src/app/components/atoms/create-account-button/CreateAccountButton.tsx
@@ -4,5 +4,6 @@ import Button from "@mui/material/Button";
 
 export const CreateAccountButton = () => {
   const handleClick = () => {};
+
   return <Button onClick={handleClick}>Create Account</Button>;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
 "use client";
 
 import { CreateAccountButton } from "@components";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(null);
-
-  // useEffect(() => {
-  //   // Fetch data from your backend server
-  //   fetch("http://localhost:5000/") // Your backend API URL
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data))
-  //     .catch((error) => console.error("Error fetching data:", error));
-  // }, []);
-
   return (
     <>
       <div className="">Welcome to Charles</div>
       <CreateAccountButton />
-      {data ? <p>{}</p> : <p>Loading...</p>}
     </>
   );
 }
