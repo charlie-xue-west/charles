7de1220695ba9eda7db7fb1e96e1b75b3d18e970
Darkflam3master <x.charlie42@gmail.com>
2025-03-20 14:44:18 -0500

    mui icon

--- src/app/components/atoms/create-account-button/CreateAccountButton.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import Button from "@mui/material/Button";
-
-export const CreateAccountButton = () => {
-  const handleClick = () => {};
-
-  return <Button onClick={handleClick}>Create Account</Button>;
-};
