91613476f83927baaeb69014975d8d2d29f7f70d
Darkflam3master <x.charlie42@gmail.com>
2025-03-24 12:26:09 -0500

    static wdith for login sign up auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from "@components";
 export default function LogInPage() {
   return (
     <main className="h-full flex justify-center items-center">
-      <AuthForm className="h-[450px]" formType="login" />
+      <AuthForm className="h-[450px] w-[381px]" formType="login" />
     </main>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from "@components";
 export default function SignUpPage() {
   return (
     <main className="h-full flex justify-center items-center">
-      <AuthForm className="h-[689px]" formType="signup" />
+      <AuthForm className="h-[689px] w-[381px]" formType="signup" />
     </main>
   );
 }
