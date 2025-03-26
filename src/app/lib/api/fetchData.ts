4704a5e9646141d9825597add1183cc6e5015b80
Darkflam3master <x.charlie42@gmail.com>
2025-03-26 11:54:54 -0500

    more robust datafetch error handling

--- src/app/lib/api/fetchData.ts
+++ src/app/lib/api/fetchData.ts
@@ -25,8 +25,22 @@ export const fetchData = async (
     return response.data as ResponseData;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
-      return error.response ? error.response.data : error.message;
-    } else {
+      if (error.code === "ERR_NETWORK" || error.message === "Network Error") {
+        return { error: "ERR_NETWORK", message: error.message };
+      }
+
+      if (error.response) {
+        return {
+          error: error.response.status,
+          message: error.response.data || "A server error has occurred",
+        };
+      }
     }
+
+    if (error instanceof Error) {
+      return { error: "Unknown Error", message: error.message };
+    }
+
+    return { error: "Unknown Error", message: "An unexpected error occurred." };
   }
 };
