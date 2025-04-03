8898f4a9cc3511cda872107c36f8f6fa49e80bd3
Darkflam3master <x.charlie42@gmail.com>
2025-04-03 15:32:01 -0500

    reduces to slices

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer } from "./reducers";
+import { authSlice } from "./slices";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: { auth: authReducer },
+    reducer: { auth: authSlice },
   });
 };
 
