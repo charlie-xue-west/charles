48f943fcb6a84e8ffaba6032b664d6aba5711712
Darkflam3master <x.charlie42@gmail.com>
2025-04-03 11:44:28 -0500

    wrap layout in storeprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Box } from "@mui/material";
 import "./globals.css";
-import { SiteHeader } from "@components";
+import { SiteHeader, StoreProvider } from "@components";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,13 +31,15 @@ export default function RootLayout({
   };
 
   return (
-    <html lang="en" className="h-full">
-      <body
-        className={`h-full ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <SiteHeader />
-        <Box sx={backgroundStyle}>{children}</Box>
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en" className="h-full">
+        <body
+          className={`h-full ${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <SiteHeader />
+          <Box sx={backgroundStyle}>{children}</Box>
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
