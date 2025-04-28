a0f0fd2491761b8ef0326b12068032bbc2e8a59d
Darkflam3master <x.charlie42@gmail.com>
2025-04-28 16:33:13 -0500

    refactor: enhance sendLogoutRequest to return structured response type

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,17 @@
 import { handleApiError } from "./error";
 import { customAxios } from "./utils";
 
+type LogoutSuccessResponse = {
+  success: boolean;
+};
+
 export async function sendLogoutRequest() {
   try {
-    const response = await customAxios.post("/auth/logout");
-    return { success: true, data: response.data };
+    const response: LogoutSuccessResponse = await customAxios.post(
+      "/auth/logout"
+    );
+
+    return response;
   } catch (error) {
     return handleApiError(error);
   }
